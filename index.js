4f3b159d50845a589794c9c5cc309dcdc044b0b0
Thaw Zin Tun <thawzintun98@gmail.com>
2024-01-10 20:34:43 +0630

    final

--- index.js
+++ index.js
@@ -1,24 +1,92 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const router = express.Router();
+const dns = require("dns");
+const mongoose = require("mongoose");
+const { URL } = require("url");
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const urlSchema = new mongoose.Schema({
+    original_url: String,
+    short_url: Number,
+});
+
+const Url = mongoose.model("Url", urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+    res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get("/api/shorturl/:urlNumber", (req, res) => {
+    const { urlNumber } = req.params;
+    if (urlNumber === 0) {
+        return res.json({
+            error: "Wrong format",
+        });
+    }
+    Url.findOne({ short_url: urlNumber }).then((result) => {
+        if (!result) {
+            return res.json({
+                error: "No short URL found for the given input",
+            });
+        }
+        return res.redirect(result.original_url);
+    });
+});
+
+app.post("/api/shorturl", (req, res) => {
+    const { url } = req.body;
+    let urlData;
+    if ((!url.startsWith("http://") && !url.startsWith("https://")) || !url) {
+        return res.json({
+            error: "Invalid URL",
+        });
+    }
+    Url.findOne({ original_url: new URL(url).origin }).then((result) => {
+        if (result) {
+            return res.json({
+                original_url: result.original_url,
+                short_url: result.short_url,
+            });
+        }
+        dns.lookup(new URL(url).hostname, async (err, address, family) => {
+            // console.log(err, address, family);
+            if (err) {
+                return res.json({
+                    error: "Invalid Hostname",
+                });
+            }
+            const totalUrls = await Url.countDocuments({});
+            urlData = {
+                original_url: new URL(url).origin,
+                short_url: totalUrls + 1,
+            };
+            Url.create(urlData).then(() => {
+                return res.json(urlData);
+            });
+        });
+    });
+});
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
